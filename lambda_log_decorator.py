8e9685e6f620b750c891fcd4a64fb15f7764df0f
tycrowe <me@tycrowe.com>
2024-05-01 02:13:49 -0400

    More cleanup.

--- lambda_log_decorator.py
+++ lambda_log_decorator.py
@@ -4,37 +4,39 @@ import time
 import json
 
 
-def logthis_lambda_function(_func=None, level=logging.INFO, custom_message=None, omit_result=True):
-
-    def decorator_logthis_lambda_function(func):
+def logthis_lambda_function(_func=None, *, level=logging.INFO, custom_message=None, omit_result=True):
+    def decorator(func):
         @functools.wraps(func)
         def wrapper(event, context):
             start_time = time.time()
+            event_id = event.get("id", "unknown") if isinstance(event, dict) else "non-dict event"
+            request_id = getattr(context, 'aws_request_id', 'unknown_request_id')
+
             try:
-                result = _func(event, context)
+                result = func(event, context)
                 log_data = {
-                    'Function': _func.__name__,
-                    'Event ID': event.get("id", "unknown"),
-                    'Request ID': context.aws_request_id,
+                    'Function': func.__name__,
+                    'Event ID': event_id,
+                    'Request ID': request_id,
                     'Execution Time': f'{time.time() - start_time} seconds',
                     'Remaining Time': f'{context.get_remaining_time_in_millis()} milliseconds',
                     'Custom Message': custom_message if custom_message else 'No custom message provided',
-                    'Output': result if not omit_result else 'Output omitted, to include set omit_result=False in decorator call.'
+                    'Output': result if not omit_result else 'Output omitted'
                 }
                 logging.log(level, json.dumps(log_data))
                 return result
             except Exception as e:
                 error_log = {
-                    'Function': _func.__name__,
-                    'Event ID': event.get("id", "unknown"),
-                    'Request ID': context.aws_request_id,
-                    'Exception': str(e)
+                    'Function': func.__name__,
+                    'Event ID': event_id,
+                    'Request ID': request_id,
+                    'Execution Time': f'{time.time() - start_time} seconds',
+                    'Exception': str(e),
+                    'Custom Message': custom_message if custom_message else 'No custom message provided'
                 }
                 logging.error(json.dumps(error_log))
                 raise e
+
         return wrapper
 
-    if _func is None:
-        return decorator_logthis_lambda_function
-    else:
-        return decorator_logthis_lambda_function(_func)
+    return decorator(_func) if _func is not None else decorator
